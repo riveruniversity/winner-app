3442ea475b5a7de251179c96128e9dc6dc5eb138
Wilhelm <wmauch@revival.com>
2025-08-02 10:27:53 -0400

    api key

--- src/js/modules/firebase-init.js
+++ src/js/modules/firebase-init.js
@@ -3,14 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebas
 import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDsmwKc7mw11sUPCn39OrJ7WWdij24AWDs",
-  authDomain: "river-winner.firebaseapp.com",
-  projectId: "river-winner",
-  storageBucket: "river-winner.firebasestorage.app",
-  messagingSenderId: "847634086851",
-  appId: "1:847634086851:web:c606fcc3219a82dac65b76"
-};
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
